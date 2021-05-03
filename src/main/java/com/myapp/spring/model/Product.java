28d494446a1cfb1f8ee2510a95da9b8b6a89efca
vijayalakshmi <gudduvijayalakshmi@gmail.com>
2021-05-03 12:15:38 +0530

    pom.xml

--- src/main/java/com/myapp/spring/model/Product.java
+++ src/main/java/com/myapp/spring/model/Product.java
@@ -62,6 +62,48 @@ public class Product {
 	public void setStarrating(double starrating) {
 		this.starrating = starrating;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + productid;
+		result = prime * result + ((productname == null) ? 0 : productname.hashCode());
+		temp = Double.doubleToLongBits(starrating);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (productid != other.productid)
+			return false;
+		if (productname == null) {
+			if (other.productname != null)
+				return false;
+		} else if (!productname.equals(other.productname))
+			return false;
+		if (Double.doubleToLongBits(starrating) != Double.doubleToLongBits(other.starrating))
+			return false;
+		return true;
+	}
+	
 	
 
 }
